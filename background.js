1ca506eccbe5b0c5084cc5829ebe719cf337879c
Anupam Mondal <a09051985@gmail.com>
2025-07-26 00:29:21 +0530

    Create background.js

---
+++ background.js
@@ -0,0 +1,6 @@
+// Background script for more complex operations
+// Currently not needed for this extension but included for completeness
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Framework Detector extension installed');
+});
